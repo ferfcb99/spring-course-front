da9a886e0662bdc199264e84e1663c0c185ea223
ivanhdzrm <ivan.hdzrm@gmail.com>
2022-12-27 17:34:15 -0600

    se agregan metodos faltantes a la interfaz IPeopleController

--- src/interfaces/without_relations/controllers/IPeopleController.ts
+++ src/interfaces/without_relations/controllers/IPeopleController.ts
@@ -1,5 +1,6 @@
 import { Observable } from "rxjs";
 import { Response } from "src/publics/Response.dto";
+import { People } from "src/entities/without_relations/People.dto";
 
 
 export interface IPeopleController<T>{
@@ -7,13 +8,11 @@ export interface IPeopleController<T>{
     getAll(): Observable<Response<T[]>>;
 	
     getById(id: number): Observable<Response<T>>;
+    
+    deleteById(id:number):Observable<Response<T>>;
 
-    /*
-	ResponseEntity<Response<T>> deleteById(Long id);
-	
-	ResponseEntity<Response<T>> createOrUpdate(T entity);
-	
-	ResponseEntity<Response<List<T>>> createOrUpdateList(List<T> entities);
-*/
+	createOrUpdate(entity:T):Observable<Response<T>>;
+
+    createOrUpdateList (entities:T[]):Observable<Response<T[]>>;
 
 }
